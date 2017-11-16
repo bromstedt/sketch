3e4b1803f1fb9b5582023e3bb18cb1809715c33a
Brian Romstedt <bromstedt322@yahoo.com>
2017-11-16 14:26:21 -0600

    Working reset

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-var input = 48;
-var size = input*input;
-var side = 600/input;
-
 makeGrid();
 
-function makeSquare(){
-
-  const square = document.createElement('div');
-  var frame = document.querySelector('#frame');
-  square.classList.add('square');
-  square.style.height = side + 'px';
-  square.style.width = side + 'px';
-  square.addEventListener('mouseover', (e)=>{
-    square.style.backgroundColor = 'black';
-  });
-  frame.appendChild(square);
-}
+var reset = document.querySelector('#reset');
+reset.addEventListener('click', (e)=>{
+  clearGrid();
+  makeGrid();
+});
 
 function makeGrid(){
 
-  for (i=0; i<size; i++){
+  var input = prompt('Squares per side?');
+  var size = input*input;
+  var side = 600/input;
+
+  function makeSquare(){
+
+    const square = document.createElement('div');
+    var frame = document.querySelector('#frame');
+    square.classList.add('square');
+    square.style.height = side + 'px';
+    square.style.width = side + 'px';
+    square.addEventListener('mouseover', (e)=>{
+      square.style.backgroundColor = 'black';
+    });
+    frame.appendChild(square);
+  }
+
+  for (i=1; i<=size; i++){
     makeSquare();
+  }
+}
+
+function clearGrid(){
+  var frame = document.getElementById('frame');
+  while (frame.firstChild){
+    frame.removeChild(frame.firstChild);
   }
 }
